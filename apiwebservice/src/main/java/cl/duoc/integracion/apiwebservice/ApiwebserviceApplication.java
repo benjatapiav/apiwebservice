6145e93750b8d2d9c38aba69ab369e72e6a39d2e
benjatapiav <benja.tapiav@duocuc.cl>
2025-05-13 23:09:39 -0400

    primer commit

---
+++ apiwebservice/src/main/java/cl/duoc/integracion/apiwebservice/ApiwebserviceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracion.apiwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiwebserviceApplication.class, args);
+	}
+
+}
---
+++ apiwebservice/src/test/java/cl/duoc/integracion/apiwebservice/ApiwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracion.apiwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
